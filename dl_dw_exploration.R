85129d011f9b321e39194259c84779e64006ced1
miguelco2000 <miguelco2000@gmail.com>
2021-07-28 06:37:24 +0200

    some comments out

--- dl_dw_exploration.R
+++ dl_dw_exploration.R
@@ -8,10 +8,10 @@ library(sparklyr)
 sc <- spark_connect(master = "local")
 
 
-spark_read_csv(sc, "data-csv/")
+# spark_read_csv(sc, "data-csv/")
 
 url <- "https://servicios.ine.es/wstempus/js/ES/DATOS_SERIE/CNTR4893?nult=1000"
-spark_read_json(sc, name = "pib", path = url, overwrite = TRUE)
+# spark_read_json(sc, name = "pib", path = url, overwrite = TRUE)
 
 
 
@@ -20,9 +20,12 @@ library(rjson)
 pib <- readLines(url)
 pib <- paste(pib, collapse = " ")
 pib <- fromJSON(pib)
-write.csv(bind_rows(pib$Data), file = here::here("data_lake", "pib", "pib_0"))
+write.csv(bind_rows(pib$Data) %>% 
+            mutate(Fecha = as.POSIXct(Fecha/1000, origin = "1970-01-01")), 
+          file = here::here("data_lake", "pib", "pib_0"))
 
 # Read a CSV file into a Spark DataFrame
 spark_read_csv(sc, "pib", file.path("file://",
                                     here::here("data_lake", "pib", "pib_0")))
 
+spark_disconnect(sc)
